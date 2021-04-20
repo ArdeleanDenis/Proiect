fc11ee268ffb1849dd750594863fa7944724689f
bogdanbbg24 <82842472+bogdanbbg24@users.noreply.github.com>
2021-04-20 03:02:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import bs4
+import requests
 
-print("radu ii campion pe sate la tras besini")
+cere = requests.get('https://www.filarmonicabanatul.ro')        #facem o variabila care face un request catre site pentru a extrage date
+
+supa = bs4.BeautifulSoup(cere.text, 'lxml')     #convertim variabila din request in beautiful soup
+hi = supa.select('.hasTooltip')         #selectam toate obiectele din cadrul clasei
+print(hi)
+for i in supa.select('.hasTooltip'):        #filtram doar elementele din subclasa a prin care putem afla titlul evenimentului si data(ziua)
+    print(i.a)
