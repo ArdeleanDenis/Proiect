a665d1e175ce04d47cea02b9b2b3f5e8568f5504
BolovanRadu <radubolovan23@gmail.com>
2021-04-21 16:48:39 +0300

    ceva test

--- main.py
+++ main.py
@@ -9,7 +9,4 @@ hi = supa.select('.hasTooltip')         #selectam toate obiectele din cadrul cla
 print(hi)
 for i in supa.select('.hasTooltip'):        #filtram doar elementele din subclasa a prin care putem afla titlul evenimentului si data(ziua)
     print(i.a)
-#ana are mere
-#ce-i gresit cu proiectu asta
-#vreau sa fac barbut
-
+#denis are mere
