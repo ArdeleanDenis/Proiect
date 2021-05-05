33425599840618ddc9dea98dec8f34d4f29d8ce5
bogdanbbg24 <82842472+bogdanbbg24@users.noreply.github.com>
2021-05-05 15:55:05 +0300

    v2

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from random import randint
-from time import sleep
 import requests
-from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
@@ -12,19 +9,42 @@ locuri = []
 categorii = []
 hitsuri = []
 headers = {"Accept-Language": "en-US,en;q=0.5"}
-pages = np.arange(5, 10, 1)
+pages = np.arange(1, 60, 1)
 for page in pages:
-    print(page)
-    page = requests.get("https://www.filarmonicabanatul.ro/index.php/component/jem/event/" + str(page) + "-concert", headers=headers)
+    # print(page)
+    page = requests.get("https://www.filarmonicabanatul.ro/index.php/component/jem/event/" + str(page) + "-concert",
+                        headers=headers)
     soup = BeautifulSoup(page.text, 'html.parser')
     spectacol = soup.find('dl', class_='jem-dl')
-    titlu = spectacol.find('dd', class_='jem-title')
-    print(titlu.text)
-    data = spectacol.find('dd', class_='jem-when')
-    print(data.text)
-    locul = spectacol.find('dd', class_='jem-where')
-    print(locul.text)
-    categoria = spectacol.find('dd', class_='jem-category')
-    print(categoria.text)
-    hits = spectacol.find('dd', class_='jem-hits')
-    print(hits.text)
+    try:
+        titlu = spectacol.find('dd', class_='jem-title')
+        spectacole.append(titlu.text)
+        # print(titlu.text)
+        data = spectacol.find('dd', class_='jem-when')
+        date.append(data.text)
+        # print(data.text)
+        locul = spectacol.find('dd', class_='jem-where')
+        locuri.append(locul.text)
+        # print(locul.text)
+        categoria = spectacol.find('dd', class_='jem-category')
+        categorii.append(categoria.text)
+        # print(categoria.text)
+        hits = spectacol.find('dd', class_='jem-hits')
+        hitsuri.append(hits.text)
+        # print(hits.text)
+    except AttributeError:
+        print("")
+# print(spectacole)
+# print(date)
+# print(locuri)
+# print(categorii)
+# print(hitsuri)
+df = pd.DataFrame({'Spactacole': spectacole,
+                   'Date': date,
+                   'Sala': locuri,
+                   'Categorii': categorii,
+                   'Hitsuri': hitsuri,
+                   })
+# print(df)
+
+df.to_csv('Filarmonica Banatulv2.csv', encoding='utf-8-sig')
