d2b84eda5246f1fa94f5beef0d55994c064dba77
ArdeleanDenis <denis-a.ardelean@student.upt.ro>
2021-05-05 14:52:01 +0300

    test

--- main.py
+++ main.py
@@ -12,19 +12,22 @@ locuri = []
 categorii = []
 hitsuri = []
 headers = {"Accept-Language": "en-US,en;q=0.5"}
-pages = np.arange(5, 10, 1)
+pages = np.arange(1, 605, 1)
 for page in pages:
-    print(page)
-    page = requests.get("https://www.filarmonicabanatul.ro/index.php/component/jem/event/" + str(page) + "-concert", headers=headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    spectacol = soup.find('dl', class_='jem-dl')
-    titlu = spectacol.find('dd', class_='jem-title')
-    print(titlu.text)
-    data = spectacol.find('dd', class_='jem-when')
-    print(data.text)
-    locul = spectacol.find('dd', class_='jem-where')
-    print(locul.text)
-    categoria = spectacol.find('dd', class_='jem-category')
-    print(categoria.text)
-    hits = spectacol.find('dd', class_='jem-hits')
-    print(hits.text)
+    try:
+        print(page)
+        page = requests.get("https://www.filarmonicabanatul.ro/index.php/component/jem/event/" + str(page) + "-concert", headers=headers)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        spectacol = soup.find('dl', class_='jem-dl')
+        titlu = spectacol.find('dd', class_='jem-title')
+        print(titlu.text)
+        data = spectacol.find('dd', class_='jem-when')
+        print(data.text)
+        locul = spectacol.find('dd', class_='jem-where')
+        print(locul.text)
+        categoria = spectacol.find('dd', class_='jem-category')
+        print(categoria.text)
+        hits = spectacol.find('dd', class_='jem-hits')
+        print(hits.text)
+    except AttributeError:
+        print(" ")
